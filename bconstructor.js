82c76744b3ff0ca71c1785fc24991f8df876c406
Lucyongondo <lucycalebs@gmail.com>
2022-07-27 16:30:20 +0300

    commit

---
+++ bconstructor.js
@@ -0,0 +1,10 @@
+function book(author, tittle, pages, readingStatus){
+    this.title= title;
+    this.author= author;
+    this.readingStatus= read;
+    this.pages= pages;
+    this.info =function(){
+        return book = this.title + " " + "by" + " " + this.author+ " " + "of" + " "+this.pages +" "+ this.readingStatus
+    };
+}
+const book1 = new book( "lucy","fortunate",1233, "read already")
